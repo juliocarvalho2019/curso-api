785e5c856b4f75722381300c228a7b0ffbaa95f4
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2022-08-17 19:33:22 -0300

    End point CREATE para user.

--- src/main/java/br/com/api/services/UserService.java
+++ src/main/java/br/com/api/services/UserService.java
@@ -1,10 +1,13 @@
 package br.com.api.services;
 
 import br.com.api.domain.User;
+import br.com.api.domain.dto.UserDTO;
 
 import java.util.List;
 
 public interface UserService {
     User findById(Integer id);
     List<User> findAll();
+    User create(UserDTO obj);
+
 }
