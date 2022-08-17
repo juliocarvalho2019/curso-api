96bf141cd2852019dd33e16be66fe15544ba5675
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2022-08-17 19:21:19 -0300

    End point findAll para user.

--- src/main/java/br/com/api/services/UserService.java
+++ src/main/java/br/com/api/services/UserService.java
@@ -2,7 +2,9 @@ package br.com.api.services;
 
 import br.com.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
-    User findById(Integer id
-    );
+    User findById(Integer id);
+    List<User> findAll();
 }
